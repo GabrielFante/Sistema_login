622a75c8735a03b9339a71bb419476d6eb9534eb
João Pedro Pereira Guerra <117650440+SmotzClean@users.noreply.github.com>
2025-09-22 20:34:44 -0300

    Update Index.php

--- src/Index.php
+++ src/Index.php
@@ -1,7 +1,7 @@
 <?php
 require_once __DIR__ . "/UserManager.php";
 
-$system = new System();
+$system = new UserManager();
 
 echo "====Caso 1: Cadastro válido====<br>";
 try {
@@ -34,4 +34,5 @@ echo "<br>";
 
 echo "====Caso 5: Cadastro de usuário com e-mail duplicado==== <br> ";
 echo $system->Register("Maria Oliveira 2", "maria@email.com", "SenhaDiferente1!");
+
 
